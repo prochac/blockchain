1f0dcc9b2c0223ddac79308992579c2ba79a2a7d
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2019-09-09 22:59:45 +0200

    4/73

--- main.go
+++ main.go
@@ -6,30 +6,89 @@ import (
 )
 
 type blockchain []block
-
-var bch blockchain
-
 type block struct {
-	previous *block
-	Value    interface{}
+	previousHash string
+	index        int64
+	transactions []transaction
 }
 
-func (b block) String() string {
-	return fmt.Sprintf("{%s %v}", b.previous, b.Value)
+func (b block) Hash() string {
+	return fmt.Sprintf("%s-%d-%v",
+		b.previousHash,
+		b.index,
+		b.transactions,
+	)
 }
+
+type transaction struct {
+	Sender    string
+	Recipient string
+	Amount    float64
+}
+
+var (
+	bch              = blockchain{{}}
+	openTransactions []transaction
+	owner            = "Tomas"
+	participants     = map[string]struct{}{
+		"Tomas": struct{}{},
+	}
+)
 
 func getLastBlock(bch blockchain) *block {
-	if len(bch) == 0 {
-		return nil
-	}
 	return &bch[len(bch)-1]
 }
 
-func addTransaction(val interface{}, prev *block) {
-	bch = append(bch, block{
-		previous: prev,
-		Value:    val,
-	})
+func addTransaction(recipient string, amount float64) {
+	addTransactionWithSender(owner, recipient, amount)
+}
+
+func addTransactionWithSender(sender, recipient string, amount float64) {
+	tx := transaction{
+		Sender:    sender,
+		Recipient: recipient,
+		Amount:    amount,
+	}
+	openTransactions = append(openTransactions, tx)
+	participants[sender] = struct{}{}
+	participants[recipient] = struct{}{}
+}
+
+func mineBlock() {
+	b := block{
+		previousHash: getLastBlock(bch).Hash(),
+		index:        int64(len(bch)),
+		transactions: openTransactions,
+	}
+	bch = append(bch, b)
+}
+
+func getTransactionValue() (string, float64) {
+	fmt.Print("Enter the recipient of the transaction: ")
+	var s string
+	if _, err := fmt.Scanf("%s", &s); err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Your transaction amount please: ")
+	var f float64
+	if _, err := fmt.Scanf("%f", &f); err != nil {
+		panic(err)
+	}
+	return s, f
+}
+
+func verifyChain() bool {
+	for i, b := range bch {
+		if i == 0 {
+			continue
+		}
+
+		if b.previousHash != bch[i-1].Hash() {
+			return false
+		}
+	}
+	return true
 }
 
 func getUserChoice() string {
@@ -40,47 +99,39 @@ func getUserChoice() string {
 	return s
 }
 
-func getTransactionValue() float64 {
-	fmt.Print("Input number: ")
-	var f float64
-	if _, err := fmt.Scanf("%f", &f); err != nil {
-		panic(err)
-	}
-	return f
-}
-
-func verifyChain() bool {
-	for i, b := range bch {
-		if i == 0 {
-			continue
-		}
-
-		if *b.previous != bch[i-1] {
-			return false
-		}
-	}
-	return true
-}
-
 func main() {
 	for {
 		fmt.Println("Please choose")
 		fmt.Println("1: Add a new transaction value")
-		fmt.Println("2: Output the clockchain blocks")
+		fmt.Println("2: Mine a new block")
+		fmt.Println("3: Output the clockchain blocks")
+		fmt.Println("4: Output participants")
 		fmt.Println("h: Manipulate the chain")
 		fmt.Println("q: Quit")
 
 		switch getUserChoice() {
 		case "1":
-			txAmount := getTransactionValue()
-			addTransaction(txAmount, getLastBlock(bch))
+			txSender, txAmount := getTransactionValue()
+			addTransaction(txSender, txAmount)
 		case "2":
+			mineBlock()
+		case "3":
 			fmt.Println(bch)
+		case "4":
+			fmt.Println(participants)
 		case "h":
 			if len(bch) < 1 {
 				continue
 			}
-			bch[0] = block{Value: 42}
+			bch[0] = block{
+				transactions: []transaction{
+					{
+						Sender:    "Chris",
+						Recipient: "max",
+						Amount:    100,
+					},
+				},
+			}
 		case "q":
 			os.Exit(0)
 		default:
